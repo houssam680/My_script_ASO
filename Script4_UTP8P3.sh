f6c60a0796317d7a1f40e39f730457e98eaaa8e4
THE_PROTECTOR150 <120113843+houssam680@users.noreply.github.com>
2023-01-31 23:14:01 +0100

    Update Script4_UTP8P3.sh
    adaptar el codigo

--- Script4_UTP8P3.sh
+++ Script4_UTP8P3.sh
@@ -13,85 +13,48 @@
 #indica en el menú anterior, y proceder a dar de baja la cuenta.
 #- En otro caso. Indicar “Error. La sintaxis correcta es
 #./Script3_UT8P3.sh alta/baja nombre apellido1 apellido2 [grupo]”
-until [[ -n $1 && -e $1 ]] ; do
-  echo " por favor introduce la ruta de archivo donde estan los usuario y ejecta el script "
-  exit
-done
 
-nombre=(`cut -d "," -f1 $1 `)
-cortador_nombre="${nombre:0:1}" ; echo "${cortador_nombre}" > /dev/null
-apellido1=(`cut -d "," -f2 $1 `)
-cortador_apellido1="${apellido1:0:2}" ; echo "${cortador_apellido1}" > /dev/null
-apellido2=(`cut -d "," -f3 $1 `)
-cortador_apellido2="${apellido2:0:2}" ; echo "${cortador_apellido2}" > /dev/null
-grupo=(`cut -d "," -f4 $1 `)
-estado=(`cut -d "," -f5 $1 `) #alta o baja
-let $2=$nombre
-let $3=$apellido1
-let $4=$apellido2
-let $5=$grupo
-let $1=$estado
+#Modificar el script anterior de manera que permita leer los datos personales de
+#varios usuarios pertenecientes a un grupo desde un fichero de texto.
+archivo=$1
+if [ $# -lt 2 ]; then
+  echo "La sintaxis correcta es ./Script3_UT8P3.sh action(alta o baja )  fichero_usuarios"
+  exit 1
+fi
+if [ "$action" == "alta" ]; then
+  while IFS=','
+    read action nombre appelido1 appelido2  groupo ; do
+    if [ "$count" -gt 0 ]; then
+    fi
+    ((count++))
+    nombre=$(echo $nombre)
+    apellido1=$(echo $appelido1)
+    apellido2=$(echo $appelido1)
 
-if [[ -n $@ ]] ; then
-    if [ $# -eq 4 ]; then
-        echo "te has pasado el siguiente parametro al script"
-        echo " --> ./Script3_UT8P3.sh alta/baja nombre apellido1 apellido2"
-        echo "entonces el script va crear el user alu${cortador_apellido1}${cortador_apellido2}${cortador_nombre}"
-        echo "y lo va asignar al grupo alu${cortador_apellido1}${cortador_apellido2}${cortador_nombre}"
-        if [ $1 == alta ]; then
-            grep -q "^alu${cortador_apellido1}${cortador_apellido2}${cortador_nombre}" /etc/group ; let x=$?
-            if [[ $x -eq 1 ]]
-            then
-                `groupadd "alu${cortador_apellido1}${cortador_apellido2}${cortador_nombre}"`
-            else
-                echo "el grupo ya existe no hace falta crear lo "
-            fi 
-            x=0
-            grep -q "alu${cortador_apellido1}${cortador_apellido2}${cortador_nombre}" /etc/shadow ; let x=$?
-            if [[ $x -eq 1 ]]
-             then
-             useradd -m -c "$nombre $apellido1 $apellido2" -g "alu${cortador_apellido1}${cortador_apellido2}${cortador_nombre}" "alu${cortador_apellido1}${cortador_apellido2}${cortador_nombre}" > /dev/null
-             else
-             echo "el usuario ya esxiste en el sistema "
-            fi
-         elif [ $1 == baja ]; then
-            userdel "alu${cortador_apellido1}${cortador_apellido2}${cortador_nombre}"
-	        groupdel "alu${cortador_apellido1}${cortador_apellido2}${cortador_nombre}" 
-        fi
+    identificativo="alu"$(echo $apellido1 | cut -c1-2)$(echo $apellido2 | cut -c1-2)$(echo $nombre | cut -c1)
+
+    if [ -z $groupo ]; then
+      sudo addgroup $identificativo
+      sudo usermod -G $identificativo $identificativo
+      echo "Usuario $identificativo creado en grupo $identificativo."
     else
-    echo " Par crear el usuario sin espicificar el grupo tienes que poner estos parametros "
-    echo " --> ./Script3_UT8P3.sh alta/baja nombre apellido1 apellido2"
+      sudo usermod -G $grupo $identificativo
+      echo "Usuario $identificativo creado en grupo $grupo."
     fi
-    if [ $# -eq 5 ]; then
-        echo "te has pasado el siguiente parametro al script"
-        echo " --> ./Script3_UT8P3.sh alta/baja nombre apellido1 apellido2 Grupo "
-        echo "entonces el script va crear el user alu${cortador_apellido1}${cortador_apellido2}${cortador_nombre}"
-        echo "y lo va asignar al grupo alu$5"
-        if [ $1 == alta ]; then
-            x=0
-            grep -q "^$5" /etc/group ; let x=$?
-            if [[ $x -eq 1 ]]
-             then
-             `groupadd "$5"`
-             else
-             echo "el grupo ya existe no hace falta crear lo "
-            fi 
-            x=0
-            grep -q "^$5" /etc/shadow ; let x=$?
-            if [[ $x -eq 1 ]]
-             then
-              useradd -m -c "$nombre $apellido1 $apellido2" -G $5 "alu${cortador_apellido1}${cortador_apellido2}${cortador_nombre}" > /dev/null
-             else
-             echo "el usuario esta en el sistema "
-            fi
-         elif [ $1 == baja ]; then
-            userdel "alu${cortador_apellido1}${cortador_apellido2}${cortador_nombre}"
-	        groupdel $5
-        fi
-    else
-        echo "Par crear el usuario y asignar lo a un grupo espicifico  al menos tienes que poner estos parametros "
-        echo "./Script3_UT8P3.sh alta/baja nombre apellido1 apellido2 [grupo]"
+  done < "$archivo"
+elif [ "$action" == "baja" ]; then
+  while IFS=','
+    read action nombre appelido1 appelido2  groupo ; do
+    if [ "$count" -gt 0 ]; then
     fi
-else
-echo "tienes que pasar al script parametros"
+    ((count++))
+    nombre=$(echo $nombre)
+    apellido1=$(echo $appelido1)
+    apellido2=$(echo $appelido1)
+
+    identificativo="alu"$(echo $apellido1 | cut -c1-2)$(echo $apellido2 | cut -c1-2)$(echo $nombre | cut -c1)
+
+    sudo userdel $identificativo
+    echo "Usuario $identificativo dado de baja."
+  done < "$archivo"
 fi
